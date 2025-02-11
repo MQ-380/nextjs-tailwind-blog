b2b8b0bbe01a40e480b3e331c2bc51278eb7feb9
Mao Quan <shmao288@gmail.com>
2025-02-11 23:12:55 +0900

    feat: add the archive page and tag list

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -4,9 +4,9 @@ const siteMetadata = {
   author: 'Subaru Izumi',
   headerTitle: 'Izumi',
   // Add the self introduction
-  description: 'A blog',
+  description: '🇯🇵 💻生活在东京的前端程序员，除了技术，还有生活。如果想要更详细了解我，请移步About页面。\n A frontend developer living in Tokyo, exploring both tech and life. For more about me, please visit the About page. \n  東京在住のフロントエンドエンジニア。技術だけでなく、生活も大切に。詳しくは About ページをご覧ください。',
   language: 'zh-CN',
-  theme: 'system', // system, dark or light
+  theme: 'light', // system, dark or light
   siteUrl: 'https://mqsh.me',
   siteRepo: 'https://github.com/MQ-380/nextjs-tailwind-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/avatar.jpeg`,

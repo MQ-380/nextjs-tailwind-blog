65a620cdf76253c11deb6ffbe28261de03203ba4
Mao Quan <shmao288@gmail.com>
2025-02-12 14:47:30 +0900

    fix: fix the images

--- components/TOC.tsx
+++ components/TOC.tsx
@@ -1,6 +1,6 @@
-"use client";
+'use client';
 
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 interface TOCProps {
   toc: {
@@ -11,43 +11,38 @@ interface TOCProps {
 }
 
 export default function TableOfContents({ toc }: TOCProps) {
-  const [activeId, setActiveId] = useState<string>("");
+  const [activeId, setActiveId] = useState<string>('');
 
   useEffect(() => {
     const handleScroll = () => {
-      const headings = document.querySelectorAll("h1, h2, h3");
+      const headings = document.querySelectorAll('h1, h2, h3');
       const headingPositions = Array.from(headings).map((heading) => ({
         id: heading.id,
         position: heading.getBoundingClientRect().top,
       }));
 
-      const activeHeading = headingPositions.find(
-        (heading) => heading.position > 0,
-      );
+      const activeHeading = headingPositions.find((heading) => heading.position > 0);
       if (activeHeading) {
         setActiveId(activeHeading.id);
       }
     };
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
   return (
-    <nav className="hidden xl:block sticky top-60 max-h-[calc(100vh-4rem)] overflow-auto">
-      <h3 className="text-lg font-bold mb-4">目录</h3>
+    <nav className="sticky top-60 hidden max-h-[calc(100vh-4rem)] overflow-auto xl:block">
+      <h3 className="mb-4 text-lg font-bold">目录</h3>
       <ul>
         {toc.map((heading) => (
-          <li
-            key={heading.value}
-            className={`${heading.depth >= 3 ? "ml-4" : ""} mb-2`}
-          >
+          <li key={heading.url} className={`${heading.depth >= 3 ? 'ml-4' : ''} mb-2`}>
             <a
               href={heading.url}
               className={`block text-sm ${
                 activeId === heading.url.slice(1)
-                  ? "text-primary-500 font-medium"
-                  : "text-gray-600 hover:text-primary-500 dark:text-gray-400 dark:hover:text-primary-500"
+                  ? 'text-primary-500 font-medium'
+                  : 'hover:text-primary-500 dark:hover:text-primary-500 text-gray-600 dark:text-gray-400'
               }`}
             >
               {heading.value}

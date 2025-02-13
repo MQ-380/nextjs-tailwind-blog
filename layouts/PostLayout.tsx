041d28db9928fd2b9c5bfbe695cf4576e9b2f346
Mao Quan <shmao288@gmail.com>
2025-02-14 00:41:38 +0900

    feat: add the about me content

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -1,14 +1,18 @@
-import { Post } from "@/.contentlayer/generated";
-import { CoreContent } from "pliny/utils/contentlayer.js";
-import { PostLinkProps } from "./PostBanner";
-import SectionContainer from "@/components/SectionContainer";
-import ScrollTop from "@/components/posts/ScrollTop";
-import siteMetadata from "@/data/siteMetadata";
-import PageTitle from "@/components/posts/PageTitle";
-import CustomLink from "@/components/Link";
-import Tag from "@/components/Tag";
-import "@/css/post.css";
-import TableOfContents from "@/components/TOC";
+import { CoreContent } from 'pliny/utils/contentlayer.js';
+
+import CustomLink from '@/components/Link';
+import SectionContainer from '@/components/SectionContainer';
+import TableOfContents from '@/components/TOC';
+import Tag from '@/components/Tag';
+import PageTitle from '@/components/posts/PageTitle';
+import ScrollTop from '@/components/posts/ScrollTop';
+
+import siteMetadata from '@/data/siteMetadata';
+
+import { Post } from '@/.contentlayer/generated';
+import '@/css/post.css';
+
+import { PostLinkProps } from './PostBanner';
 
 interface LayoutProps {
   content: CoreContent<Post>;
@@ -18,18 +22,13 @@ interface LayoutProps {
 }
 
 const postDateTemplate: Intl.DateTimeFormatOptions = {
-  weekday: "long",
-  year: "numeric",
-  month: "long",
-  day: "numeric",
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
 };
 
-export default function PostLayout({
-  content,
-  next,
-  prev,
-  children,
-}: LayoutProps) {
+export default function PostLayout({ content, next, prev, children }: LayoutProps) {
   const { date, title, tags, toc, readingTime } = content;
 
   return (
@@ -37,17 +36,14 @@ export default function PostLayout({
       <ScrollTop />
       <article>
         <div>
-          <header className="pt-6 xl:flex xl:pb-6 justify-between">
+          <header className="justify-between pt-6 xl:flex xl:pb-6">
             <div className="space-y-1 text-left xl:w-2/3">
               <dl className="space-y-10">
                 <div>
                   <dt className="sr-only">Published on</dt>
-                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                  <dd className="text-base leading-6 font-medium text-gray-500 dark:text-gray-400">
                     <time dateTime={date}>
-                      {new Date(date).toLocaleDateString(
-                        siteMetadata.locale,
-                        postDateTemplate,
-                      )}
+                      {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
                     </time>
                   </dd>
                 </div>
@@ -58,7 +54,7 @@ export default function PostLayout({
             <div>
               {tags && (
                 <div className="py-4 xl:py-8">
-                  <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
+                  <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
                     Tags
                   </h2>
                   <div className="flex flex-wrap">
@@ -76,28 +72,18 @@ export default function PostLayout({
             {/* 内容区域 */}
             <div className="xl:w-5/7">
               <div className="divide-y divide-gray-200 dark:divide-gray-700">
-                <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">
-                  {children}
-                </div>
+                <div className="prose dark:prose-invert max-w-none pt-10 pb-8">{children}</div>
 
                 {/* 将 footer 移到内容区域内 */}
                 <footer>
-                  <div className="divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 ">
+                  <div className="divide-gray-200 text-sm leading-5 font-medium dark:divide-gray-700">
                     {(next || prev) && (
                       <div className="flex justify-between py-4 xl:block xl:space-y-8 xl:py-8">
                         {prev && prev.path && (
-                          <PostLink
-                            path={prev.path}
-                            title={prev.title}
-                            isPrev={true}
-                          />
+                          <PostLink path={prev.path} title={prev.title} isPrev={true} />
                         )}
                         {next && next.path && (
-                          <PostLink
-                            path={next.path}
-                            title={next.title}
-                            isPrev={false}
-                          />
+                          <PostLink path={next.path} title={next.title} isPrev={false} />
                         )}
                       </div>
                     )}
@@ -109,7 +95,7 @@ export default function PostLayout({
             {/* 目录区域 */}
             {toc.length > 0 && (
               <aside className="hidden xl:block xl:w-2/7">
-                <div className="sticky top-60 overflow-y-auto max-h-[calc(100vh-6rem)]">
+                <div className="sticky top-60 max-h-[calc(100vh-6rem)] overflow-y-auto">
                   <TableOfContents toc={toc} />
                 </div>
               </aside>
@@ -121,14 +107,10 @@ export default function PostLayout({
   );
 }
 
-const PostLink = ({
-  path,
-  title,
-  isPrev,
-}: PostLinkProps & { isPrev: boolean }) => (
+const PostLink = ({ path, title, isPrev }: PostLinkProps & { isPrev: boolean }) => (
   <div>
-    <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
-      {isPrev ? "Previous Article" : "Next Article"}
+    <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
+      {isPrev ? 'Previous Article' : 'Next Article'}
     </h2>
     <div className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
       <CustomLink href={`/${path}`}>{title}</CustomLink>

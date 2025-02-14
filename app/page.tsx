956188656a63a07a4f0167f274c5a1bb8ebb1b83
Mao Quan <shmao288@gmail.com>
2025-02-14 10:49:55 +0900

    fix: fix the serviceworker

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer.js';
 
-import { unregister } from '@/components/serviceWorker';
-
 import { allPosts } from '@/.contentlayer/generated';
 
 import { Main } from './Main';
@@ -9,6 +7,5 @@ import { Main } from './Main';
 export default async function Page() {
   const sortedPosts = sortPosts(allPosts);
   const posts = allCoreContent(sortedPosts);
-  unregister();
   return <Main posts={posts} />;
 }
